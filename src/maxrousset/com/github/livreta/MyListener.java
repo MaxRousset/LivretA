cf08484a1e01fd366c59304cd85d1a16d3c59b0a
MaxRousset <maxencerousset717@gmail.com>
2018-11-03 11:27:18 +0100

    remove useless import

--- src/maxrousset/com/github/livreta/MyListener.java
+++ src/maxrousset/com/github/livreta/MyListener.java
@@ -7,7 +7,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 
 import com.earth2me.essentials.api.Economy;
 import com.earth2me.essentials.api.NoLoanPermittedException;
